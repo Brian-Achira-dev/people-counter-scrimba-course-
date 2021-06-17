ba46fcb3780bdbd105d2c42a44ba649a3ce566fa
Brian-Achira-dev <verahmakori98@gmail.com>
2021-06-17 08:56:40 +0300

    updated

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ console.log(countEl)
 let count = 0;
 function increase(){
     count = count+1;
-    console.log(count);
-    countEl.innerHTML  = count;
+    //console.log(count);
+    countEl.innerText  = count;
 }
 
 function decrease(){
     count = count-1;
-    console.log(count);
-    countEl.innerHTML  = count;
+    //console.log(count);
+    countEl.innerText  = count;
 }
+
+let savEl = document.getElementById('save-el');
+
+
+function save(){
+	let var1 = count + " - ";
+	savEl.textContent += var1;
+    count = 0;
+    countEl.textContent = count;
+}
+
+
